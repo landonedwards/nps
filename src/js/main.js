b3dfa1dbfb4796c36c7ffe3c00609a59bba87f01
landonedwards <lando.s.edwards@gmail.com>
2025-09-18 20:16:51 -0600

    completed nps W01 assignment

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,25 @@
 import { getParkData } from "./parkService.mjs";
 
 const parkData = getParkData();
+
+const disclaimer = document.querySelector(".disclaimer > a");
+disclaimer.href = parkData.url;
+disclaimer.innerHTML = parkData.fullName;
+
+const title = document.querySelector("title");
+title.innerHTML = parkData.fullName;
+
+const heroImage = document.querySelector(".park-hero-img");
+heroImage.src = parkData.images[0].url;
+
+function parkInfoTemplate(info) {
+    return `
+    <a href="/" class="park-name"><span>${info.name}</span></a>
+    <p class="park-location">
+      <span>${info.designation}</span>
+      <span>${info.states}</span>
+    </p>`;
+}
+
+const parkDetails = document.querySelector(".park-info");
+parkDetails.innerHTML = parkInfoTemplate(parkData);
